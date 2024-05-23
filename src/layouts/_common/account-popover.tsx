dcefd4f9af63a06053b27b0037b375cdbecf7707
Lucky <117964815+pondev1031@users.noreply.github.com>
2024-05-23 04:23:48 +0200

    update

--- src/layouts/_common/account-popover.tsx
+++ src/layouts/_common/account-popover.tsx
@@ -78,7 +78,7 @@ export default function AccountPopover() {
       <CustomPopover open={popover.open} onClose={popover.onClose} sx={{ width: 200, p: 0 }}>
         <Box sx={{ p: 2, pb: 1.5 }}>
           <Typography variant="subtitle2" noWrap>
-            {`${user?.name} (${user?.role?.label})`}
+            {`${user?.name}`}
           </Typography>
 
           <Typography variant="body2" sx={{ color: 'text.secondary' }} noWrap>
